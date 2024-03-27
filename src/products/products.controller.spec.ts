84945a8f0f1a7647ef51ca9bb80259942d6b1077
Aung Myat Moe <aungmyatmoe834@gmail.com>
2024-03-28 04:29:52 +0630

    test: fix proudct test

--- src/products/products.controller.spec.ts
+++ src/products/products.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
+import { productProviders } from './product.providers';
 
 describe('ProductsController', () => {
   let controller: ProductsController;
@@ -8,7 +9,7 @@ describe('ProductsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductsController],
-      providers: [ProductsService],
+      providers: [ProductsService, ...productProviders],
     }).compile();
 
     controller = module.get<ProductsController>(ProductsController);
