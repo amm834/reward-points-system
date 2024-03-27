5367d2a238be3c713b0881c9bb79bc8c46c5a090
Aung Myat Moe <aungmyatmoe834@gmail.com>
2024-03-28 04:23:02 +0630

    feat: add products CRUD

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,27 @@
-export class Product {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({
+  name: 'products',
+})
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 500 })
+  name: string;
+
+  @Column('text', {
+    nullable: true,
+  })
+  description?: string;
+
+  @Column('int', {
+    default: 0,
+  })
+  price: number;
+
+  @Column('int', {
+    default: 0,
+  })
+  stock_units: number;
+}
